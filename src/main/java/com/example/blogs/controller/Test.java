6510796167e81dc0adf372ae5a739590be3c8457
munaihe <898998369@qq.com>
2019-05-18 15:19:36 +0800

    modify READMEtest controller

---
+++ src/main/java/com/example/blogs/controller/Test.java
@@ -0,0 +1,11 @@
+package com.example.blogs.controller;
+
+import com.alibaba.druid.support.spring.stat.SpringStatUtils;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("ddddddddddddddddddd");
+    }
+}
